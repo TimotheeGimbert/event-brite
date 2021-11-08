e1fe545647930da0cd95d7bafcf379b79d97ac73
TimotheeGimbert <timothee.gimbert@free.fr>
2021-11-08 17:26:03 +0100

    adds validations and seed

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,42 @@
 class Event < ApplicationRecord
+
   belongs_to :administrator, class_name: "User"
   has_many :users, through: :attendances
+
+  validates :title,
+    presence: true,
+    lenght: { minimum: 5, maximum: 140 }
+
+  validates :start_date, 
+    presence: true, 
+    :start_date_cannot_be_in_the_past
+
+  def start_date_cannot_be_in_the_past
+    if start_date <= Datetime.now
+      errors.add(:start_date, "La date de début de l'évênement doit se situer dans le futur !")
+    end
+  end
+
+  validates :duration, 
+    presence: true, 
+    numericality: { only_integer: true, greater_than: 0 }, 
+    :duration_should_be_multiple_of_5
+
+  def duration_should_be_multiple_of_5
+    if duration % 5 != 0
+      errors.add(:duration, "Un évênement doit avoir une durée multiple de 5 minutes !")
+    end
+  end
+
+  validates :price,
+    presence: true,
+    numericality: { only_integer: true, greater_than: 1, less_than: 1000 }
+
+  validates :description,
+    presence: true,
+    lenght: { minimum: 5, maximum: 140 }
+
+  validates :location,
+    presence: true
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tim = User.create(
+  first_name:'Tim',
+  last_name: 'Gim',
+  email: 'tim33270@yopmail.com',
+  encrypted_password: 'blabla',
+  description: "Description de l'utilisateur Tim"
+)
+puts tim
+
+nono = User.create(
+  first_name:'Nono',
+  last_name: 'Ma',
+  email: 'nono33270@yopmail.com',
+  encrypted_password: 'blublu',
+  description: "Description de l'utilisateur Nono"
+)
+puts nono
