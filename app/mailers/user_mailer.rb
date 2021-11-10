ab1d3e22657bfde4b54039bf2c704e6d2d17b289
TimotheeGimbert <timothee.gimbert@free.fr>
2021-11-10 23:50:14 +0100

    removes auto mail send

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,13 +4,13 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user                                              
     @url  = 'http://monsite.fr/login'                        
-    mail(to: @user.email, subject: 'Bienvenue chez nous !')
+    #mail(to: @user.email, subject: 'Bienvenue chez nous !')
   end
 
   def confirmation_attendance_email(event_admin)
     @user = event_admin                                             
     @url  = 'http://monsite.fr/login'                               
-    mail(to: @user.email, subject: 'Confirmation de participation!') 
+    #mail(to: @user.email, subject: 'Confirmation de participation!') 
     puts @user
     puts @user.email
   end
