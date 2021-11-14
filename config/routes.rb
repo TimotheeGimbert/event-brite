7bba19a5fe26f533dcacd3634c4f0a7e01b628c6
TimotheeGimbert <timothee.gimbert@free.fr>
2021-11-14 16:07:10 +0100

    shows attendants if admin

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :static_pages
   resources :event
   resources :attendance
+  resources :user, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
